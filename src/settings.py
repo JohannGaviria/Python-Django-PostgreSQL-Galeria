e620cd693b63d99d261f72756c22918d4251b525
JohannGaviria <johannmgr@gmail.com>
2024-07-05 14:44:22 -0500

    feat(app): crear las apliaciones Django
    - Crear la apliación `users`
    - Crear la apliación `albums`
    - Crear la apliación `images`
    - Crear la apliación `comments`
    - Crear la apliación `likes`
    - Crear la apliación `tags`
    - Crear la apliación `analytics`

--- src/settings.py
+++ src/settings.py
@@ -42,6 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'albums',
+    'images',
+    'comments',
+    'likes',
+    'tags',
+    'analytics',
 ]
 
 MIDDLEWARE = [
