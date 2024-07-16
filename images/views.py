1c71e5a8d0a39f6c374a7ab736d2834d9c7197c5
JohannGaviria <johannmgr@gmail.com>
2024-07-16 14:54:26 -0500

    feat(images): Crear la obtención y creación de imagenes
    - Crear la obtención de imagenes
    - Crear la creación de imagenes

--- images/views.py
+++ images/views.py
@@ -3,28 +3,69 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Image
+from .serializers import ImageSerializer
 
 
 # Lógica para obtener y crear imagenes
 @api_view(['GET', 'POST'])
 @authentication_classes([TokenAuthentication])
-@permission_classes(IsAuthenticated)
+@permission_classes([IsAuthenticated])
 def image_list_create(request):
     # Método GET para obtener las imagenes
     if request.method == 'GET':
+        # Obtiene las imagenes del usuario
+        images = Image.objects.filter(user=request.user).order_by('id')
+
+        # Verifica que existan imagenes
+        if not images.exists():
+            # Respuesta de error
+            return Response({
+                'status': 'errors',
+                'message': 'Validation failed',
+                'errors': {
+                    'images': [
+                        'you do not have any images uploaded'
+                    ]
+                }
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Serializa los datos
+        serializer = ImageSerializer(images, many=True)
+
         # Respuesta exitosa
         return Response({
             'status': 'success',
             'message': 'Images loaded successfully',
+            'data': {
+                'images': serializer.data
+            }
         }, status=status.HTTP_200_OK)
     
     # Método POST para crear una imagen
     if request.method == 'POST':
-        # Respuesta exitosa
+        # Serialize los datos recibidos en la solicitud
+        serializer = ImageSerializer(data=request.data)
+
+        # Verifica que los datos son válidos
+        if serializer.is_valid():
+            # Guarda la imagen
+            serializer.save()
+            # Respuesta exitosa
+            return Response({
+                'status': 'success',
+                'message': 'Image uploaded successfully',
+                'data': {
+                    'image': serializer.data
+                }
+            }, status=status.HTTP_201_CREATED)
+        
+        # Respuesta de error
         return Response({
-            'status': 'success',
-            'message': 'Image uploaded successfully',
-        }, status=status.HTTP_201_CREATED)
+            'status': 'errors',
+            'message': 'Validation failed',
+            'errors': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Lógica para el detalle de imagenes
