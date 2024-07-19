a0a09efd3ad0ff9361f34823569a86d33af66396
JohannGaviria <johannmgr@gmail.com>
2024-07-19 16:15:59 -0500

    feat(comments): Crear la obtención y creación de comentarios
    - Crear la obtención de comentarios de imagenes especificas
    - Crear la creación de comentarios de imagenes especificas

--- comments/views.py
+++ comments/views.py
@@ -3,28 +3,70 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Comment
+from .serializers import CommentSerializer
 
 
 # Lógica para obtener y crear comentarios
 @api_view(['GET', 'POST'])
 @authentication_classes([TokenAuthentication])
-@permission_classes(IsAuthenticated)
+@permission_classes([IsAuthenticated])
 def comment_list_create(request, image_id):
     # Método GET para obtener los comentarios
     if request.method == 'GET':
+        # Obtiene los comentarios de la imagen
+        comments = Comment.objects.filter(image=image_id).order_by('id')
+
+        # Verifica que existan imagenes
+        if not comments.exists():
+            # Respuesta de error
+            return Response({
+                'status': 'errors',
+                'message': 'Validation failed',
+                'errors': {
+                    'comments': [
+                        'The image has no comments'
+                    ]
+                }
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Serializa los datos
+        serializer = CommentSerializer(comments, many=True)
+
         # Respuesta exitosa
         return Response({
             'status': 'success',
             'message': 'Comments loaded successfully',
+            'data': {
+                'comments': serializer.data
+            }
         }, status=status.HTTP_200_OK)
     
     # Método POST para crear un comentario
     if request.method == 'POST':
-        # Respuesta exitosa
+        # Serializa los datos recibidos en la solicitud
+        serializer = CommentSerializer(data=request.data)
+
+        # Verifica que los datos son válidos
+        if serializer.is_valid():
+            # Guarda el comentario
+            serializer.save()
+
+            # Respuesta exitosa
+            return Response({
+                'status': 'success',
+                'message': 'Comment added successfully',
+                'data': {
+                    'comment': serializer.data
+                }
+            }, status=status.HTTP_201_CREATED)
+        
+        # Respuesta de error
         return Response({
-            'status': 'success',
-            'message': 'Comment added successfully',
-        }, status=status.HTTP_201_CREATED)
+            'status': 'errors',
+            'message': 'Validation failed',
+            'errors': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Lógica para el detalle de comentarios
